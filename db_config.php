148ae5642c48a39a67f81d6d7a6dd25aaebac480
Kamran Zafar <kamranzafar4343@gmail.com>
2024-10-31 23:30:46 +0500

    added env file

--- db_config.php
+++ db_config.php
@@ -1,14 +1,20 @@
 <?php
 
- $hostName = "localhost";
- $userName = "root";
- $password = "";
- $dbName = "bestamzd_service_app";
- $conn= new mysqli($hostName,$userName,$password,$dbName);
- if($conn){
-    // echo "connected";	
- }else{
-    // echo "not connected";
- }
- 
- ?>
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$hostName = $_ENV['hostName'];
+$userName = $_ENV['userName'];
+$password = $_ENV['password'];
+$dbName = $_ENV['dbName'];
+
+$conn = new mysqli($hostName, $userName, $password, $dbName);
+if ($conn) {
+   // echo "connected";	
+} else {
+   // echo "not connected";
+}
